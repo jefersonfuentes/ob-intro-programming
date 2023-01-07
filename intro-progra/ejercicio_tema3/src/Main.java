7549c004834470175913f3df7d19b073d9660a19
Jeferson <jeferson.fg710@gmail.com>
2023-01-07 17:37:22 -0600

    Comments added

--- intro-progra/ejercicio_tema3/src/Main.java
+++ intro-progra/ejercicio_tema3/src/Main.java
@@ -1,27 +1,41 @@
 public class Main {
     public static void main(String[] args) {
+        // Primera parte
+
+        // Función suma
         suma(20, 40, 60);
 
-        Coche miCoche = new Coche();
+        // Segunda parte
 
-        miCoche.sumarPuertas();
-        miCoche.sumarPuertas();
-        System.out.println("El numero de puertas es: " + miCoche.numeroDePuertas);
+        Coche miCoche = new Coche(); // Instancia del objeto Coche
+
+        // Se invoca el metodo sumar puertas cuatro veces por ende
+        // numeroDePuertas = 0 + 4
+        miCoche.agregarPuertas();
+        miCoche.agregarPuertas();
+        miCoche.agregarPuertas();
+        miCoche.agregarPuertas();
+        System.out.println("El numero de puertas es: " + miCoche.numPuertas);
 
     }
 
+    // Función suma, recibe 3 paramateros de tipo entero
     public static void suma(int a, int b, int c){
+        //La función no retorna un valor por eso es void y crea su proceso internamente
         int resultado;
         resultado = a + b + c;
         System.out.println(resultado);
     }
 }
 
+    // Declaración de la clase coche
 class Coche {
-    public int numeroDePuertas = 4;
+    // Variable numerica tipo int para guardar en memoria la cantidad de puertas del coche
+    public int numPuertas = 0;
 
-    public void sumarPuertas(){
-
-        this.numeroDePuertas++;
+    // Método para incrementar las puertas
+    public void agregarPuertas(){
+        // La palabra reservada this permite invocar la variable dentro de la función
+        this.numPuertas++;
     }
 }
